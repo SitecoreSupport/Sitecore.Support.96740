8c91015a5a5acfb8dc3e8154d5137e07797ff6d4
dk\ivd <ivd@sitecore.net>
2017-04-28 14:39:41 +0800

    #96740: Check exclusion for each dependent indexable

---
+++ src/Sitecore.Support.96740/SitecoreItemCrawler.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Sitecore.Support.ContentSearch
+{
+    using System.Linq;
+    using Sitecore.ContentSearch;
+
+    public class SitecoreItemCrawler : Sitecore.ContentSearch.SitecoreItemCrawler
+    {
+        public SitecoreItemCrawler()
+        {
+        }
+
+        public SitecoreItemCrawler(IIndexOperations indexOperations) : base(indexOperations)
+        {
+        }
+
+        protected override void UpdateDependents(IProviderUpdateContext context, [NotNull]SitecoreIndexableItem indexable)
+        {
+            foreach (var uniqueId in GetIndexingDependencies(indexable).Where(i => !this.IsExcludedFromIndex(i, true)))
+            {
+                if (!this.CircularReferencesIndexingGuard.IsInProcessedList(uniqueId, this, context))
+                    this.Update(context, uniqueId);
+            }
+        }
+
+    }
+}
